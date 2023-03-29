d804dcaf2d6bd039143f3be3717b2c8c58a493e1
lcasesa <casesa@wisc.edu>
2023-03-29 18:15:08 -0500

    ch 9 update
    updated css

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ window.onload = setMap();
 function setMap() {
 
     //map frame dimensions
-    var width = 960,
-        height = 460;
+    var width = 1200,
+        height = 960;
 
     //create new svg container for the map
     var map = d3
@@ -16,14 +16,15 @@ function setMap() {
         .attr("width", width)
         .attr("height", height);
 
-    //create Albers equal area conic projection centered on France
+    //create Albers equal area conic projection centered on California
     var projection = d3.geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
+        .center([14.91, 38.59])
+        .rotate([134.91, 0, 0])
+        .parallels([18.68, 38.61])
+        .scale(1489.90)
         .translate([width / 2, height / 2]);
 
+    //path to convert projection
     var path = d3.geoPath().projection(projection);
 
     //use Promise.all to parallelize asynchronous data loading
@@ -39,13 +40,25 @@ function setMap() {
         var csvData = data[0],
             california = data[1],
             usa = data[2];
-        console.log(csvData);
-        console.log(usa);
-        console.log(california);
-
-        //translate europe TopoJSON
+        
+        //translate states TopoJSON
         var states = topojson.feature(usa, usa.objects.ne_110m_admin_1_states_provinces),
             ca_counties = topojson.feature(california, california.objects.CA_Counties_TIGER2016);
 
+        //add all states to map
+        var lower48 = map.append("path")
+            .datum(states)
+            .attr("class", "states")
+            .attr("d", path);
+
+        //add california counties to map
+        var counties = map.selectAll(".regions")
+            .data(ca_counties)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
     }
 };
