c4be9e193acdf13576e48009d079276d1ecbf1a8
lcasesa <casesa@wisc.edu>
2023-04-18 15:57:39 -0500

    finished chapter 10
    - completed

--- js/main.js
+++ js/main.js
@@ -160,7 +160,13 @@
     function setChart(csvData, colorScale) {
         //chart frame dimensions
         var chartWidth = window.innerWidth * 0.425,
-            chartHeight = 460;
+            chartHeight = 473,
+            leftPadding = 25,
+            rightPadding = 2,
+            topBottomPadding = 5,
+            chartInnerWidth = chartWidth - leftPadding - rightPadding,
+            chartInnerHeight = chartHeight - topBottomPadding * 2,
+            translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
         //create a second svg element to hold the bar chart
         var chart = d3.select("body")
@@ -169,65 +175,66 @@
             .attr("height", chartHeight)
             .attr("class", "chart");
 
-        //create a scale to size bars proportionally to frame
+        //create a rectangle for chart background fill
+        var chartBackground = chart.append("rect")
+            .attr("class", "chartBackground")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+
+        //create a scale to size bars proportionally to frame and for axis
         var yScale = d3.scaleLinear()
-            .range([0, chartHeight])
-            .domain([0, 105]);
+            .range([463, 0])
+            .domain([0, 100]);
 
         //set bars for each province
-        var bars = chart.selectAll(".bars")
+        var bars = chart.selectAll(".bar")
             .data(csvData)
             .enter()
             .append("rect")
             .sort(function (a, b) {
-                return a[expressed] - b[expressed]
+                return b[expressed] - a[expressed]
             })
             .attr("class", function (d) {
-                return "bars " + d.adm1_code;
+                return "bar " + d.adm1_code;
             })
-            .attr("width", chartWidth / csvData.length - 1)
+            .attr("width", chartInnerWidth / csvData.length - 1)
             .attr("x", function (d, i) {
-                return i * (chartWidth / csvData.length);
+                return i * (chartInnerWidth / csvData.length) + leftPadding;
             })
-            .attr("height", function (d) {
-                return yScale(parseFloat(d[expressed]));
+            .attr("height", function (d, i) {
+                return 463 - yScale(parseFloat(d[expressed]));
             })
-            .attr("y", function (d) {
-                return chartHeight - yScale(parseFloat(d[expressed]));
-            }).style("fill", function (d) {
+            .attr("y", function (d, i) {
+                return yScale(parseFloat(d[expressed])) + topBottomPadding;
+            })
+            .style("fill", function (d) {
                 return colorScale(d[expressed]);
             });
 
-        //annotate bars with attribute value text
-        var numbers = chart.selectAll(".numbers")
-            .data(csvData)
-            .enter()
-            .append("text")
-            .sort(function (a, b) {
-                return a[expressed] - b[expressed]
-            })
-            .attr("class", function (d) {
-                return "numbers " + d.adm1_code;
-            })
-            .attr("text-anchor", "middle")
-            .attr("x", function (d, i) {
-                var fraction = chartWidth / csvData.length;
-                return i * fraction + (fraction - 1) / 2;
-            })
-            .attr("y", function (d) {
-                return chartHeight - yScale(parseFloat(d[expressed])) + 15;
-            })
-            .text(function (d) {
-                return d[expressed];
-            });
-
-        //below Example 2.8...create a text element for the chart title
+        //create a text element for the chart title
         var chartTitle = chart.append("text")
-            .attr("x", 20)
+            .attr("x", 40)
             .attr("y", 40)
             .attr("class", "chartTitle")
-            .text("Percentage of adults aged 20+ with diabetes " + expressed[3] + " per county");
+            .text("Number of Variable " + expressed[3] + " in each region");
 
+        //create vertical axis generator
+        var yAxis = d3.axisLeft()
+            .scale(yScale);
+
+        //place axis
+        var axis = chart.append("g")
+            .attr("class", "axis")
+            .attr("transform", translate)
+            .call(yAxis);
+
+        //create frame for chart border
+        var chartFrame = chart.append("rect")
+            .attr("class", "chartFrame")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
     };
 
 })();
--- js/new.js
+++ js/new.js
@@ -1,49 +1,73 @@
-function joinData(franceRegions,csvData){
-    //loop through csv to assign each set of csv attribute values to geojson region
-        for (var i=0; i<csvData.length; i++){
-            var csvRegion = csvData[i]; //the current region
-            var csvKey = csvRegion.adm1_code; //the CSV primary key
+//function to create coordinated bar chart
+function setChart(csvData, colorScale) {
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 460;
 
-            //loop through geojson regions to find correct region
-            for (var a=0; a<franceRegions.length; a++){
-                var geojsonProps = franceRegions[a].properties; //the current region geojson properties
-                var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart");
 
-                //where primary keys match, transfer csv data to geojson properties object
-                if (geojsonKey == csvKey){
+    //create a scale to size bars proportionally to frame
+    var yScale = d3.scaleLinear()
+        .range([0, chartHeight])
+        .domain([0, 105]);
 
-                    //assign all attributes and values
-                    attrArray.forEach(function(attr){
-                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
-                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
-                    });
-                };
-            };
-        };
-        return franceRegions;
-}
+    //set bars for each province
+    var bars = chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function (a, b) {
+            return a[expressed] - b[expressed]
+        })
+        .attr("class", function (d) {
+            return "bars " + d.adm1_code;
+        })
+        .attr("width", chartWidth / csvData.length - 1)
+        .attr("x", function (d, i) {
+            return i * (chartWidth / csvData.length);
+        })
+        .attr("height", function (d) {
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function (d) {
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        }).style("fill", function (d) {
+            return colorScale(d[expressed]);
+        });
 
-function joinData(ca_counties,csvData){ 
-    //loop through csv to assign each set of csv attribute values to geojson region
-    for (var i = 0; i < csvData.length; i++) {
-        var csvRegion = csvData[i]; //the current region
-        var csvKey = csvRegion.adm1_code; //the CSV primary key
+    //annotate bars with attribute value text
+    var numbers = chart.selectAll(".numbers")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .sort(function (a, b) {
+            return a[expressed] - b[expressed]
+        })
+        .attr("class", function (d) {
+            return "numbers " + d.adm1_code;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function (d, i) {
+            var fraction = chartWidth / csvData.length;
+            return i * fraction + (fraction - 1) / 2;
+        })
+        .attr("y", function (d) {
+            return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+        })
+        .text(function (d) {
+            return d[expressed];
+        });
 
-        //loop through geojson regions to find correct region
-        for (var a = 0; a < ca_counties.length; a++) {
-            var geojsonProps = ca_counties[a].properties; //the current region geojson properties
-            var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+    //below Example 2.8...create a text element for the chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 20)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text("Percentage of adults aged 20+ with diabetes " + expressed[3] + " per county");
 
-            //where primary keys match, transfer csv data to geojson properties object
-            if (geojsonKey == csvKey) {
-
-                //assign all attributes and values
-                attrArray.forEach(function (attr) {
-                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
-                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
-                });
-            };
-        };
-    };
-    return ca_counties;
-}
+};
